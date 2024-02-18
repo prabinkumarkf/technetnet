840efa9feeaba0175ed318263bee92cfebc210c0
prabinkumarkf <prabinkumarkf@gmail.com>
2024-02-18 23:26:42 +0530

    st

--- custom.js
+++ custom.js
@@ -1,11 +1,1 @@
-const { colors: defaultColors } = require('tailwindcss/defaultTheme')
-
-module.exports = {
-    "theme": {
-        "colors": defaultColors + {
-            "custom-yellow": {
-                "500": "#eee",
-            }
-        },
-    },
-};
+ 
